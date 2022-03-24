d0436a3505acfa687e356216384476908e14e86a
Polyakov Daniil <70587587+Polliakoff@users.noreply.github.com>
2022-03-24 19:25:46 +0300

    user+migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
+from statistics import mode
 from django.db import models
+
+class User(models.Model):
+    login = models.CharField('Логин', max_length=20)
+    password = models.CharField('Пароль',max_length=100)
+
+    def __str__(self):
+        return self.login
+
+
 
 # Create your models here.
