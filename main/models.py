82495c61170db0f64dda40af54d533e89e193e34
Polyakov Daniil <70587587+Polliakoff@users.noreply.github.com>
2022-03-25 22:07:16 +0300

    bd

--- main/models.py
+++ main/models.py
@@ -1,6 +1,13 @@
 from statistics import mode
 from django.db import models
+from django.contrib.auth.models import User
 
+class Avatar_of_choice(models.Model):
+    name = models.CharField('Название', max_length=50)
+    #usr = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,default='blank')
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
