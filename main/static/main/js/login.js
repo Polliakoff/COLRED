8747c1e39dbbcd3b69f1e79d36b5e9a62acaec9f
Shilakin am I <IvanTheCraziest@gmail.com>
2022-03-30 00:05:23 +0300

    fix

--- main/static/main/js/login.js
+++ main/static/main/js/login.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('#id_username').attr('placeholder', 'Логин');
     $('#id_password').attr('placeholder', 'Пароль');
-    $('button.conf_button').css('margin-top', '53px');
+    $('.conf_button_b').css('margin-top', '53px');
 });
