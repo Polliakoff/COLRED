58e7f48227df196c58a64f4fe662626c0f60a2cf
Shilakin am I <IvanTheCraziest@gmail.com>
2022-05-11 02:14:36 +0300

    2

--- main/static/main/js/list.js
+++ main/static/main/js/list.js
@@ -1,104 +1,113 @@
+
+
 function welp(t){
     switch (t) {
-        case "1":
-            return "-5";
-            break;
-        case "2":
-        case "3":
-            return "-4";
-            break;
-        case "4":
-        case "5":
-            return "-3";
-            break;
-        case "6":
-        case "7":
-            return "-2";
-            break;
         case "8":
+            return "1d-2";
+            break;
         case "9":
-            return "-1";
+            return "1d-1";
             break;
         case "10":
+            return "1d";
+            break;
         case "11":
-            return "+0";
+            return "1d+1";
             break;
         case "12":
+            return "1d+2";
+            break;
         case "13":
-            return "+1";
+            return "2d-1";
             break;
         case "14":
-        case "15":
-            return "+2";
+            return "2d";
             break;
         case "16":
-        case "17":
-            return "+3";
+            return "2d+1";
             break;
-        case "18":
-        case "19":
-            return "+4";
+    }
+}
+
+function welp2(t){
+    switch (t) {
+        case "8":
+            return "1d-3";
             break;
-        case "20":
-        case "21":
-            return "+5";
+        case "9":
+            return "1d-2";
             break;
-        case "22":
-        case "23":
-            return "+6";
+        case "10":
+            return "1d-2";
             break;
-        case "24":
-        case "25":
-            return "+7";
+        case "11":
+            return "1d-1";
+            break;
+        case "12":
+            return "1d-1";
+            break;
+        case "13":
+            return "1d";
+            break;
+        case "14":
+            return "1d";
+            break;
+        case "16":
+            return "1d+1";
             break;
     }
 }
 
 
 $(document).ready(function () {
-    t = $("#DX").val()
-    $(".DX").text(welp(t))
-
     t = $("#STR").val()
-    $(".STR").text(welp(t))
+    $("#dmg_2").text(welp(t))
+    $("#dmg_1").text(welp2(t))
 
-    t = $("#CON").val()
-    $(".CON").text(welp(t))
+    points = ($("#STR").val()-10)*10 + ($("#DX").val()-10)*20 + ($("#IN").val()-10)*20 + ($("#CON").val()-10)*10
+    points += ($("#id_hp").val()-$("#CON").val())*5 + ($("#WIS").val()-$("#IN").val())*5 + ($("#CHR").val()-$("#IN").val())*5
+    points += ($("#id_speed").val()- ($("#id_hp").val() -(- $("#DX").val()))/4)*4*5
 
-    t = $("#IN").val()
-    $(".IN").text(welp(t))
+    points += ($("#id_armour_class").val()- $("#id_speed").val())*5
 
-    t = $("#WIS").val()
-    $(".WIS").text(welp(t))
+    $("#class_lvl").text(points)
 
-    t = $("#CHR").val()
-    $(".CHR").text(welp(t))
-
-    t = Math.floor($("#id_xp").val()/1000)
-    $("#class_lvl").text(t)
 });
 
 $("input").change(function () {
-    t = $("#DX").val()
-    $(".DX").text(welp(t))
-
     t = $("#STR").val()
-    $(".STR").text(welp(t))
+    $("#dmg_2").text(welp(t))
+    $("#dmg_1").text(welp2(t))
 
-    t = $("#CON").val()
-    $(".CON").text(welp(t))
+    points = ($("#STR").val()-10)*10 + ($("#DX").val()-10)*20 + ($("#IN").val()-10)*20 + ($("#CON").val()-10)*10
+    points += ($("#id_hp").val()-$("#CON").val())*5 + ($("#WIS").val()-$("#IN").val())*5 + ($("#CHR").val()-$("#IN").val())*5
+    points += ($("#id_speed").val()- ($("#id_hp").val() -(- $("#DX").val()))/4)*4*5
 
-    t = $("#IN").val()
-    $(".IN").text(welp(t))
+    points += ($("#id_armour_class").val()- $("#id_speed").val())*5
 
-    t = $("#WIS").val()
-    $(".WIS").text(welp(t))
+    $("#class_lvl").text(points)
 
-    t = $("#CHR").val()
-    $(".CHR").text(welp(t))
+    // $(".STR").text(welp(t))
+    // t = $("#DX").val()
+    // $(".DX").text(welp(t))
+
+    // t = $("#STR").val()
+    // $(".STR").text(welp(t))
+
+    // t = $("#CON").val()
+    // $(".CON").text(welp(t))
+
+    // t = $("#IN").val()
+    // $(".IN").text(welp(t))
+
+    // t = $("#WIS").val()
+    // $(".WIS").text(welp(t))
+
+    // t = $("#CHR").val()
+    // $(".CHR").text(welp(t))
     
-    t = Math.floor($("#id_xp").val()/1000)
-    $("#class_lvl").text(t)
+    // t = Math.floor($("#id_xp").val()/1000)
+    // $("#class_lvl").text(t)
 });
 
 
